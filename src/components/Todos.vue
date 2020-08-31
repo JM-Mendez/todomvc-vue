52a0a1cb2295b0975f73c3462a43c2b9931f7358
John Mendez <john.manuel.mendez@gmail.com>
2020-08-31 17:57:56 -0400

    add checkbox to todo input

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,13 +2,39 @@
   <section class="todoapp">
     <header class="header">
       <h1>todos</h1>
-      <input class="new-todo" placeholder="What needs to be done?" autofocus />
+      <input
+        v-model="todoText"
+        class="new-todo"
+        placeholder="What needs to be done?"
+        autofocus
+        autocomplete="false"
+      />
     </header>
+    <section class="main">
+      <input
+        id="toggle-all"
+        v-model="toggleSelectAll"
+        class="toggle-all"
+        type="checkbox"
+      />
+      <label html-for="toggle-all" :title="title">{{ title }}</label>
+    </section>
   </section>
 </template>
 
 <script>
 export default {
-  name: 'Todos'
+  name: 'Todos',
+  data: function() {
+    return {
+      todoText: '',
+      toggleSelectAll: false
+    }
+  },
+  computed: {
+    title: function() {
+      return `Mark all as ${this.selectAll ? 'active' : 'completed'}`
+    }
+  }
 }
 </script>
